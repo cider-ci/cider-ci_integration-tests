a3c7887f86247765a9b9b5333bfde86d3975404d
Thomas Schank <DrTom@schank.ch>
2015-05-31 21:15:00 +0200

    Finish up run example on CI

--- spec/helpers/demo_executor.rb
+++ spec/helpers/demo_executor.rb
@@ -2,6 +2,8 @@ module Helpers
   module DemoExecutor
     class << self
       def configure_demo_executor
+        port = Integer(ENV['EXECUTOR_HTTP_PORT'].present? && 
+                       ENV['EXECUTOR_HTTP_PORT'] || '8883')
         Helpers::ConfigurationManagement.invoke_ruby \
           'Executor.find_or_initialize_by(name: "DemoExecutor")' \
           '.update_attributes!(base_url: "http://localhost:8883") '
