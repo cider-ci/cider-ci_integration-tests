e3670541e970582c76a4923568df22a3d0a0cc84
Thomas Schank <DrTom@schank.ch>
2015-08-09 16:26:43 +0200

    Test accepted_repositories in push and pull mode

---
+++ spec/features/dispatching_and_executing/accepted-repositories_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+require 'shared/push_and_pull'
+
+shared_examples :accepted_repositories do
+  it 'The job will not be dispatched unless ' \
+    'the git_url is one of the accepted ones' do
+    job_name = 'Environment Variables Demo'
+    sign_in_as 'adam'
+    Helpers::DemoExecutor.set_accepted_repositories \
+      ['http://nonsense.com/blah']
+    run_job_on_last_commit job_name
+    job_path = current_path
+    sleep 30
+    visit job_path
+    wait_for_job_state job_name, 'pending'
+    Helpers::DemoExecutor.set_accepted_repositories \
+      ['http://nonsense.com/blah', Helpers::DemoRepo.git_url]
+    sleep 5
+    visit job_path
+    wait_for_job_state job_name, 'passed'
+  end
+end
+
+describe 'Dispatching and running in ', type: :feature do
+  include_context :run_in_push_and_pull_mode,
+    :accepted_repositories
+end
