3f7fda8bdeaf5f867518b90d70016e086472a1ce
Thomas Schank <DrTom@schank.ch>
2015-09-15 21:07:43 +0200

    Reconfigure executor setup (id rather than name)

--- spec/helpers/demo_executor.rb
+++ spec/helpers/demo_executor.rb
@@ -6,7 +6,8 @@ module Helpers
         port = Integer(ENV['EXECUTOR_HTTP_PORT'].present? &&
                        ENV['EXECUTOR_HTTP_PORT'] || '8883')
         Helpers::ConfigurationManagement.invoke_ruby \
-          'Executor.find_or_initialize_by(name: "DemoExecutor")' \
+          'Executor.find_or_initialize_by(name: "DemoExecutor", ' \
+              'id: "35cff40c-b4f8-4ca3-9217-d49c9c35f375")' \
           ".update_attributes!(base_url: 'http://localhost:#{port}') "
       end
 
