4174c7b51c20b7c4161a09c7df4e2aa1a11f7a64
Thomas Schank <DrTom@schank.ch>
2015-11-14 21:08:17 +0100

    Stabilize API Browser tests with sleep

--- spec/helpers/misc.rb
+++ spec/helpers/misc.rb
@@ -32,6 +32,7 @@ module Helpers
     end
 
     def api_click_on_relation_method(rel_name, method_name)
+      sleep 0.5
       find('tr.relation-row .rel-name', text: /^\s*#{rel_name}\s*$/) \
         .find(:xpath, '..').find('a', text: method_name).click
     end
