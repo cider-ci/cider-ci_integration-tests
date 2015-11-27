90eea24b35b43f64a6fa22144fc665059919e444
Thomas Schank <DrTom@schank.ch>
2015-11-27 15:40:26 +0100

    Improve test

--- spec/api/resources/commits_spec.rb
+++ spec/api/resources/commits_spec.rb
@@ -31,14 +31,12 @@ describe 'commits' do
   end
 
   describe 'commit' do
-
     it 'includes the expected keys' do
       expect(
         authenticated_json_roa_client.get.relation('commits')
         .get(tree_id: '6451155c768c3ee04c9b014dd15b3c4356741635')
         .collection.first.get.data.keys.map(&:to_sym)
       ).to include :tree_id, :committer_email, :committer_date, :author_email, :author_date
-
     end
 
     it 'relates to jobs' do
@@ -47,10 +45,6 @@ describe 'commits' do
         .get(tree_id: '6451155c768c3ee04c9b014dd15b3c4356741635')
         .collection.first.get.relation(:jobs)
       ).to be_a JSON_ROA::Client::Relation
-
     end
-
   end
-
 end
-
--- spec/api/resources/jobs_spec.rb
+++ spec/api/resources/jobs_spec.rb
@@ -2,7 +2,6 @@ require 'api/shared'
 require 'set'
 
 describe 'jobs, ' do
-
   before :all do
     Helpers::Misc.reset_and_configure
     wait_until do
@@ -11,84 +10,77 @@ describe 'jobs, ' do
     end
   end
 
-  describe 'creating and querying jobs'  do
-
+  describe 'creating and querying jobs' do
     it 'works' do
-
-      tree_id= authenticated_json_roa_client.get.relation(:commits) \
-        .get(repository_url:  Helpers::DemoRepo.git_url,
-             branch_head: 'master') \
-        .collection.first.get.data[:tree_id]
+      tree_id = authenticated_json_roa_client.get.relation(:commits) \
+                .get(repository_url:  Helpers::DemoRepo.git_url,
+                     branch_head: 'master') \
+                .collection.first.get.data[:tree_id]
 
       job_id = authenticated_json_roa_client.get.relation(:create_job) \
-        .post({},{tree_id: tree_id, key: 'environment-variables'}.to_json,
-             content_type: 'application/json').data[:id]
+               .post({}, { tree_id: tree_id, key: 'environment-variables' }.to_json,
+                 content_type: 'application/json').data[:id]
 
       wait_until do
         authenticated_json_roa_client.get.relation(:job) \
           .get(id: job_id).data[:state] == 'passed'
       end
 
-      expect( authenticated_json_roa_client.get.relation(:jobs) \
+      expect(authenticated_json_roa_client.get.relation(:jobs) \
              .get(tree_id: tree_id,
                   repository_url: Helpers::DemoRepo.git_url,
                   key: 'environment-variables',
                   branch_descendants: 'master',
                   branch_head: 'master',
-                  state: 'passed').collection.first ).to be
+                  state: 'passed').collection.first).to be
 
-      expect( authenticated_json_roa_client.get.relation(:jobs) \
+      expect(authenticated_json_roa_client.get.relation(:jobs) \
              .get(tree_id: '1111111111111111111111111111111111111111',
                   repository_url: Helpers::DemoRepo.git_url,
                   key: 'environment-variables',
                   branch_descendants: 'master',
                   branch_head: 'master',
-                  state: 'passed').collection.first ).not_to be
+                  state: 'passed').collection.first).not_to be
 
-      expect( authenticated_json_roa_client.get.relation(:jobs) \
+      expect(authenticated_json_roa_client.get.relation(:jobs) \
              .get(tree_id: tree_id,
-                  repository_url: (Helpers::DemoRepo.git_url + "/X"),
+                  repository_url: (Helpers::DemoRepo.git_url + '/X'),
                   key: 'environment-variables',
                   branch_descendants: 'master',
                   branch_head: 'master',
-                  state: 'passed').collection.first ).not_to be
+                  state: 'passed').collection.first).not_to be
 
-      expect( authenticated_json_roa_client.get.relation(:jobs) \
+      expect(authenticated_json_roa_client.get.relation(:jobs) \
              .get(tree_id: tree_id,
                   repository_url: Helpers::DemoRepo.git_url,
                   key: 'blah',
                   branch_descendants: 'master',
                   branch_head: 'master',
-                  state: 'passed').collection.first ).not_to be
+                  state: 'passed').collection.first).not_to be
 
-      expect( authenticated_json_roa_client.get.relation(:jobs) \
+      expect(authenticated_json_roa_client.get.relation(:jobs) \
              .get(tree_id: tree_id,
                   repository_url: Helpers::DemoRepo.git_url,
                   key: 'environment-variables',
                   branch_descendants: 'masterX',
                   branch_head: 'master',
-                  state: 'passed').collection.first ).not_to be
+                  state: 'passed').collection.first).not_to be
 
-      expect( authenticated_json_roa_client.get.relation(:jobs) \
+      expect(authenticated_json_roa_client.get.relation(:jobs) \
              .get(tree_id: tree_id,
                   repository_url: Helpers::DemoRepo.git_url,
                   key: 'environment-variables',
                   branch_descendants: 'master',
                   branch_head: 'masterX',
-                  state: 'passed').collection.first ).not_to be
+                  state: 'passed').collection.first).not_to be
 
-
-      expect( authenticated_json_roa_client.get.relation(:jobs) \
+      expect(authenticated_json_roa_client.get.relation(:jobs) \
              .get(tree_id: tree_id,
                   repository_url: Helpers::DemoRepo.git_url,
                   key: 'environment-variables',
                   branch_descendants: 'master',
                   branch_head: 'master',
-                  state: 'failed').collection.first ).not_to be
-
+                  state: 'failed').collection.first).not_to be
     end
-
   end
-
 end
-
