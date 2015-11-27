31d3ec28b9004c2a45f080f5a9e253d6806c6d8e
Thomas Schank <DrTom@schank.ch>
2015-11-27 14:50:51 +0100

    Improve test

--- spec/features/aggregate-state_satisfy-last_spec.rb
+++ spec/features/aggregate-state_satisfy-last_spec.rb
@@ -5,7 +5,7 @@ feature 'Aggregate State Feature for satisfy-last', type: :feature do
     'Aggregate State with `satisfy-last`'
   end
 
-  scenario 'The becomes failed when the last trial failed' do
+  scenario 'The job becomes failed when the last trial failed' do
     setup_signin_waitforcommits
     run_job_on_last_commit job_name
     wait_for_job_state job_name, 'failed'
@@ -26,12 +26,23 @@ feature 'Aggregate State Feature for satisfy-last', type: :feature do
     visit job_path
     expect(find('#job-info .state')).to have_content 'passed'
 
-    # retry again (will fail) and check that the job is still passed
+    # retry again
     visit last_trial_path
     click_on 'Retry'
     expect(page).to have_content 'A new trial has been created.'
-    wait_until { find('#trial-info .state').has_content? 'failed' }
+    last_trial_path= current_path
+
+    # the job will become 'executing' even though it has passed before
     visit job_path
-    expect(find('#job-info .state')).to have_content 'failed'
+    wait_for_job_state job_name, 'executing'
+
+    # let us wait for the trial to fail
+    visit last_trial_path
+    wait_until { find('#trial-info .state').has_content? 'failed' }
+
+    # then the job will become failed to
+    visit job_path
+    wait_for_job_state job_name, 'failed'
+
   end
 end
