595fd6481d35bf4229f7b1e36504cf7b54b5ef0d
Thomas Schank <DrTom@schank.ch>
2016-02-27 10:45:36 +0100

    Fix executor pw

--- spec/helpers/demo_executor.rb
+++ spec/helpers/demo_executor.rb
@@ -5,11 +5,12 @@ module Helpers
       def configure_demo_executor
         port = Integer(ENV['EXECUTOR_HTTP_PORT'].present? &&
                        ENV['EXECUTOR_HTTP_PORT'] || '8883')
-        Helpers::ConfigurationManagement.invoke_ruby \
-          'Executor.find_or_initialize_by(name: "DemoExecutor", ' \
-              'id: "35cff40c-b4f8-4ca3-9217-d49c9c35f375")' \
-          ".update_attributes!(base_url: 'http://localhost:#{port}'" \
-          ", upload_tree_attachments: true)"
+        Helpers::ConfigurationManagement.invoke_ruby <<-RUBY.strip_heredoc
+          Executor.find_or_initialize_by(name: "DemoExecutor",
+              id: "ENV['EXECUTOR_ID']")
+            .update_attributes!(base_url: 'http://localhost:#{port}' ,
+              upload_tree_attachments: true)"
+          RUBY
       end
 
       def set_accepted_repositories(repos)
