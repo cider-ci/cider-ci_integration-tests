9ef0f90d94094f31c5967397f452c169beaed0cc
Thomas Schank <DrTom@schank.ch>
2016-02-27 11:01:22 +0100

    Fix config executor

--- spec/helpers/demo_executor.rb
+++ spec/helpers/demo_executor.rb
@@ -5,12 +5,13 @@ module Helpers
       def configure_demo_executor
         port = Integer(ENV['EXECUTOR_HTTP_PORT'].present? &&
                        ENV['EXECUTOR_HTTP_PORT'] || '8883')
-        Helpers::ConfigurationManagement.invoke_ruby <<-RUBY.strip_heredoc
+        config_executor_rb = %Q<
           Executor.find_or_initialize_by(name: "DemoExecutor",
-              id: "ENV['EXECUTOR_ID']")
+              id: "#{ENV['EXECUTOR_ID']}")
             .update_attributes!(base_url: 'http://localhost:#{port}' ,
-              upload_tree_attachments: true)"
-          RUBY
+              upload_tree_attachments: true)
+            >.squish
+        Helpers::ConfigurationManagement.invoke_ruby  config_executor_rb
       end
 
       def set_accepted_repositories(repos)
