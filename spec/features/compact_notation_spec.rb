2295a6c8518997cfae4d37b8e52545cb5f43a879
Thomas Schank <DrTom@schank.ch>
2016-05-23 19:31:32 +0200

    Fix job spec

--- spec/features/compact_notation_spec.rb
+++ spec/features/compact_notation_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-describe 'the job "Introduction Demo and Example" ', type: :feature do
+describe 'the job "Introduction Demo and Example Job" ', type: :feature do
   before :all do
     setup_signin_waitforcommits
   end
   it 'passes' do
     sign_in_as 'admin'
-    run_job_on_last_commit 'Introduction Demo and Example'
-    wait_for_job_state 'Introduction Demo and Example', 'passed'
+    run_job_on_last_commit 'Introduction Demo and Example Job'
+    wait_for_job_state 'Introduction Demo and Example Job', 'passed'
   end
 end
--- spec/features/pushing-job-statuses_spec.rb
+++ spec/features/pushing-job-statuses_spec.rb
@@ -26,8 +26,8 @@ describe "Sending job statuses to a GitHub compatible API endpoint.",
       find('input#repository_foreign_api_repo').set "test-repo"
       find("form *[type='submit']").click
 
-      run_job_on_last_commit 'Introduction Demo and Example'
-      wait_for_job_state 'Introduction Demo and Example', 'passed'
+      run_job_on_last_commit 'Introduction Demo and Example Job'
+      wait_for_job_state 'Introduction Demo and Example Job', 'passed'
 
       written_data = YAML.load_file('tmp/last-status-post.yml').with_indifferent_access
       expect(written_data[:auth_token]).to be== 'test-token'
