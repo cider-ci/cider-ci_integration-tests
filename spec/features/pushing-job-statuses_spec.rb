7810654298ae08d70206060fc5f05965783366c3
Thomas Schank <DrTom@schank.ch>
2016-06-12 09:25:02 +0200

    Improve pushing-job-statuses test

--- spec/features/pushing-job-statuses_spec.rb
+++ spec/features/pushing-job-statuses_spec.rb
@@ -29,6 +29,12 @@ describe "Sending job statuses to a GitHub compatible API endpoint.",
       run_job_on_last_commit 'Introduction Demo and Example Job'
       wait_for_job_state 'Introduction Demo and Example Job', 'passed'
 
+      wait_until 10 do
+        File.exist?('tmp/last-status-post.yml') &&
+          (YAML.load_file('tmp/last-status-post.yml')
+           .with_indifferent_access[:body][:state] == 'success')
+      end
+
       written_data = YAML.load_file('tmp/last-status-post.yml').with_indifferent_access
       expect(written_data[:auth_token]).to be== 'test-token'
       expect(written_data[:body][:state]).to be== 'success'
