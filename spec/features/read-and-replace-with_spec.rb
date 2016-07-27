fa5d04ebf32c26c1ad16b51075956b195d4c47d2
Thomas Schank <DrTom@schank.ch>
2016-07-27 21:58:01 +0200

    Add read-and-replace-with_spec.rb

---
+++ spec/features/read-and-replace-with_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'Read and Replace With Demo', type: :feature  do
+  before :all do
+    setup_signin_waitforcommits
+  end
+  it 'passes when being run' do
+    run_job_on_last_commit 'Read and Replace With Demo'
+    wait_for_job_state 'Read and Replace With Demo', 'passed'
+  end
+end
