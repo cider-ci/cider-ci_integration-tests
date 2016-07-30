67ed647b51ec36f0fe14cb7672f795b3248bba6d
Thomas Schank <DrTom@schank.ch>
2016-07-30 09:07:22 +0200

    Add include spec

---
+++ spec/features/include_spec.rb
@@ -0,0 +1,13 @@
+
+require 'spec_helper'
+
+describe 'the job "Include Demo" ', type: :feature do
+  before :all do
+    setup_signin_waitforcommits
+  end
+  it 'passes' do
+    sign_in_as 'admin'
+    run_job_on_last_commit 'Include Demo'
+    wait_for_job_state 'Include Demo', 'passed'
+  end
+end
