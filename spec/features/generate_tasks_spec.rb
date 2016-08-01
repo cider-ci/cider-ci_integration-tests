0b18ab27f33388d7ab93600e62eaca8cca836236
Thomas Schank <DrTom@schank.ch>
2016-08-01 11:48:14 +0200

    Extend "Generate Tasks" spec

--- spec/features/generate_tasks_spec.rb
+++ spec/features/generate_tasks_spec.rb
@@ -4,9 +4,31 @@ describe 'the job "Generate Tasks Demo" ', type: :feature do
   before :all do
     setup_signin_waitforcommits
   end
-  it 'passes' do
-    sign_in_as 'admin'
-    run_job_on_last_commit 'Generate Tasks Demo'
-    wait_for_job_state 'Generate Tasks Demo', 'passed'
+
+  context "Generate Tasks Demo" do
+    it 'passes and the generated task is present' do
+
+      sign_in_as 'admin'
+      run_job_on_last_commit 'Generate Tasks Demo'
+      wait_for_job_state 'Generate Tasks Demo', 'passed'
+
+      # check that generated task is there
+      expect(page).to have_content "specs/test1_spec.sh"
+    end
   end
+
+  context "Generate Tasks for Submodules Demo" do
+    it 'passes and the generated task is present' do
+
+      sign_in_as 'admin'
+      run_job_on_last_commit 'Generate Tasks for Submodules Demo'
+      wait_for_job_state 'Generate Tasks for Submodules Demo', 'passed'
+
+      # check that generated task is there
+      expect(page).to have_content "data/007"
+    end
+  end
+
+
 end
+
