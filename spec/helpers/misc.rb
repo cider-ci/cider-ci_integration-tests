3b006896de04df0b9660f31b73e6710f561d8a6a
Thomas Schank <DrTom@schank.ch>
2016-10-23 23:37:01 +0200

    Use wait_until within click_on_first

--- spec/helpers/misc.rb
+++ spec/helpers/misc.rb
@@ -20,6 +20,7 @@ module Helpers
     end
 
     def click_on_first(locator, options = {})
+      wait_until(3){first(:link_or_button, locator, options)}
       first(:link_or_button, locator, options).click
     end
 
