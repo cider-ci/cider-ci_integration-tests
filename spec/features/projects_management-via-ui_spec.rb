f9e5b894af4a21044488e9c52774cf676b1b5e26
Thomas Schank <DrTom@schank.ch>
2017-01-29 18:43:48 +0100

    Adjustments for ui2 and repositories merge to server

--- spec/features/projects_management-via-ui_spec.rb
+++ spec/features/projects_management-via-ui_spec.rb
@@ -5,27 +5,24 @@ feature 'Admin manages Repositories', type: :feature do
     Helpers::ConfigurationManagement.invoke_ruby 'PgTasks.truncate_tables() && "OK"'
     Helpers::Users.create_users
   end
-
   scenario 'Create a repository, editing and deleting it' do
     sign_in_as 'admin'
     visit '/'
     click_on 'Projects'
-    wait_until { page.has_content? 'Add a new project'}
+    wait_until(5) { page.has_content? 'Add a new project'}
     click_on 'Add a new project'
     find('input#git_url').set 'https://github.com/cider-ci/cider-ci_demo-project-bash.git'
     find('input#name').set 'TestRepo'
     find('input#name').set 'TestRepo'
     click_on 'Submit'
-    wait_until { page.has_content?  /Project\s+"TestRepo"/ }
+    wait_until(5) { page.has_content?  /Project\s+"TestRepo"/ }
     wait_until { page.has_content?  /Edit/ }
     click_on 'Edit'
     find('input#name').set 'UpdatedName'
-    # set other fields too
-    # binding.pry
     click_on 'Submit'
-    wait_until { page.has_content?  /Project\s+"UpdatedName"/ }
+    wait_until(5) { page.has_content?  /Project\s+"UpdatedName"/ }
     click_on 'Delete'
-    wait_until { page.has_content?  /Add a new project/ }
-    expect(page).not_to have_content "UpdatedName"
+    wait_until(5) { page.has_content?  /Add a new project/ }
+    expect(find(".table-projects")).not_to have_content "UpdatedName"
   end
 end
