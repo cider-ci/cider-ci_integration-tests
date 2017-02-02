10f7133b52f9fac1009d2f5d5cb88af9514dbbf9
Thomas Schank <DrTom@schank.ch>
2017-02-02 20:25:07 +0100

    Enable and fix feature test

--- spec/features/aggregate-state_validation_spec.rb
+++ spec/features/aggregate-state_validation_spec.rb
@@ -5,19 +5,13 @@ feature 'Aggregate State Feature for Validation typo', type: :feature do
     'Aggregate State with Typo'
   end
 
-  scenario 'It fails with an task error' do
+  scenario 'It defects with an valiation error' do
     setup_signin_waitforcommits
     run_job_on_last_commit job_name
-    wait_for_job_state job_name, 'failed'
+    wait_for_job_state job_name, 'defective'
 
-    # the task has been aborted
-    click_on 'task1'
-    expect(find('#task-info .state')).to have_content 'aborted'
     # and the page shows a validation error
     expect(page).to have_content 'Validation Error'
 
-    # there are no trials at all
-    find('#trials table tbody')
-    expect(all('#trials table tbody tr')).to be_empty
   end
 end
