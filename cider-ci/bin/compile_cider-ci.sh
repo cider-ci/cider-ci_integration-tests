c84b7313b2f830011402c3d676c48764ba5e5015
Thomas Schank <DrTom@schank.ch>
2023-04-10 19:49:13 +0200

    Fix more CI stuff #psh

--- cider-ci/bin/compile_cider-ci.sh
+++ cider-ci/bin/compile_cider-ci.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
-set -eux
+set -euo pipefail
 cd server
-export LEIN_SNAPSHOTS_IN_RELEASE=yes
-# SELF_DIGEST=$(git log -n 1 HEAD --pretty=%T)
-# UTILS_DIGEST=$(cd ../clj-utils &&  git log -n 1 HEAD --pretty=%T)
-# LEIN_DEV_PLUGIN_DIGEST=$(cd ../lein-dev-plugin &&  git log -n 1 HEAD --pretty=%T)
-# CONFIG_DIGEST=$(cd .. && git ls-tree HEAD -- config | openssl dgst -sha1 | cut -d ' ' -f 2)
-# DIGEST="${SELF_DIGEST}_${UTILS_DIGEST}_${LEIN_DEV_PLUGIN_DIGEST}_${CONFIG_DIGEST}"
 DIGEST=$(cd .. &&  git log -n 1 HEAD --pretty=%T)
 LEIN_UBERJAR_FILE="/tmp/cider-ci_${DIGEST}.jar"
 if [ -f "${LEIN_UBERJAR_FILE}" ];then
   echo " ${LEIN_UBERJAR_FILE} exists"
 else
-  lein uberjar
+  ./bin/uberjar
   mv "target/cider-ci.jar" "${LEIN_UBERJAR_FILE}"
 fi
 mkdir -p target
